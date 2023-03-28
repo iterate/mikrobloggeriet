5c538796b08a4ec49fbbbdbc68e0d52ae82b6392
Teodor Heggelund <git@teod.eu>
2023-03-28 21:22:50 +0200

    olorm set-repo-path

--- cli/src/olorm/cli.clj
+++ cli/src/olorm/cli.clj
@@ -1,18 +1,33 @@
 (ns olorm.cli
-  (:require [babashka.fs :as fs]
-            [clojure.edn :as edn]
-            [babashka.cli :as cli]))
+  (:require
+   [babashka.cli :as cli]
+   [babashka.fs :as fs]
+   [clojure.string :as str]))
+
+
+(defn config-folder [] (str (fs/xdg-config-home) "/olorm"))
+(defn config-file [] (str (config-folder) "/config.edn"))
 
 (declare subcommands)
-
-(defn olorm-set-repo-path [{:keys [opts]}]
-  (prn 'olorm-set-repo-path))
 
 (defn olorm-help [{:keys [opts]}]
   (prn 'olorm))
 
+(defn olorm-set-repo-path [{:keys [opts]}]
+  (let [repo-path (:repo-path opts)]
+    (when-not repo-path
+      (println (str/trim "
+Usage:
+
+    olorm set-repo-path REPO_PATH
+"))
+      (System/exit 1))
+    (let [repo-path (fs/absolutize repo-path)]
+      (fs/create-dirs (config-folder))
+      (spit (config-file) (prn-str {:repo-path (str repo-path)})))))
+
 (def subcommands
-  [{:cmds ["set-repo-path"] :fn olorm-set-repo-path}
+  [{:cmds ["set-repo-path"] :fn olorm-set-repo-path :args->opts [:repo-path]}
    {:cmds []                :fn olorm-help}])
 
 (defn -main [& args]
