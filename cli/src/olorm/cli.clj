333fa46cc806c8b53068017ca71661f0b4a51359
Teodor Heggelund <git@teod.eu>
2023-03-28 21:05:53 +0200

    subcommands

--- cli/src/olorm/cli.clj
+++ cli/src/olorm/cli.clj
@@ -1,4 +1,19 @@
-(ns olorm.cli)
+(ns olorm.cli
+  (:require [babashka.fs :as fs]
+            [clojure.edn :as edn]
+            [babashka.cli :as cli]))
 
-(defn -main [& _args]
+(declare subcommands)
+
+(defn olorm-set-repo-path [{:keys [opts]}]
+  (prn 'olorm-set-repo-path))
+
+(defn olorm-help [{:keys [opts]}]
   (prn 'olorm))
+
+(def subcommands
+  [{:cmds ["set-repo-path"] :fn olorm-set-repo-path}
+   {:cmds []                :fn olorm-help}])
+
+(defn -main [& args]
+  (cli/dispatch subcommands args))
