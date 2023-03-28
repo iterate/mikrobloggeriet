b7e720aa9aff7e16c0343ebecf0a786f9108f9df
Teodor Heggelund <git@teod.eu>
2023-03-28 21:50:39 +0200

    iki think

--- lib/vendor/iki/api.clj
+++ lib/vendor/iki/api.clj
@@ -1,2 +1,9 @@
 (ns iki.api)
 
+;; vendored iki API namespace
+;;
+;; the idea is to prototype functionality close to the user before "collecting"
+;; together in a central artifact.
+;;
+;; I tried making an IKI library earlier, but it was too early. Too much hassle
+;; with versions, too little progress.
