eb132c7b1d299248ab8b0de8b539cdb4bc517cbb
Teodor Heggelund <git@teod.eu>
2023-04-16 14:31:17 +0200

    Decision: pandoc html string into hiccup tree

--- serve/src/olorm/devui.clj
+++ serve/src/olorm/devui.clj
@@ -43,7 +43,37 @@
 ;; ... or do I want hiccup?
 ;; not sure.
 ;; how do I want this to integrate with the rest of the system?
-;; what am I doing (really) in OGGPOW?
+
+;; Q: what am I doing (really) in OGGPOW?
+;;
+;; A: I just generate an HTML string and slap it into the Hiccup tree.
+;;
+;;     (defn page [req]
+;;       (let [{:keys [slug]} (:params req)
+;;             page {:slug slug}
+;;             time-before-convert (now-ms)
+;;             page-html (->html page)
+;;             time-after-convert (now-ms)]
+;;         (page/html5
+;;          [:head (hiccup.page/include-css "/style.css")]
+;;          [:body
+;;           [:div {:class style/content-container}
+;;            [:a {:href "/"} "home"]
+;;            [:h2 slug]
+;;            [:pre (with-out-str (pprint/pprint (->meta page)))]
+;;            [:p "Inferred builder: " [:code  (pr-str (infer-builder page))]]
+;;            [:p "Page created in " [:em  (- time-after-convert time-before-convert)] " ms."]
+;;            [:div page-html]]])))
+;;
+;; Okay, let's to the same here.
+
+
+
+
+
+
+
+
 
 ^
 {:nextjournal.clerk/visibility {:code :hide}}
