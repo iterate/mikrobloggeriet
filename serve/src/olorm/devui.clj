55301d191df8927feb456d19c0b4aaa3493f1f86
Teodor Heggelund <git@teod.eu>
2023-04-16 17:27:29 +0200

    Delete unused code

--- serve/src/olorm/devui.clj
+++ serve/src/olorm/devui.clj
@@ -4,10 +4,9 @@
   (:require
    [nextjournal.clerk :as clerk]
    [olorm.lib :as olorm]
-   [iki.api :as iki]
-   [babashka.fs :as fs]))
+   [iki.api :as iki]))
 
-;; ## Session 1
+;; ## Looking at things, eg http requests
 
 (defonce xx (atom nil))
 
@@ -17,63 +16,7 @@
  (for [[h v] (:headers @xx)]
    {:header h :value v}))
 
-123123123
-
-(let [repo-path  ".."]
-  (->> (fs/list-dir (fs/canonicalize (str repo-path "/p")))
-       (map fs/file-name)
-       (map str)
-       (map olorm/slug->olorm)
-       (filter :olorm)))
-
-#_
-(let [repo-path  ".."]
-  (fs/canonicalize
-   (str repo-path "/p"))
-  #_#_#_
-  (map str)
-  (map olorm/slug->olorm)
-  (filter :olorm))
-
-;; ## Session 2
-
-(defn olorm->html [& _args])
-
-(olorm->html {:slug "olorm-1" :repo-path ".."})
-
-;; ... or do I want hiccup?
-;; not sure.
-;; how do I want this to integrate with the rest of the system?
-
-;; Q: what am I doing (really) in OGGPOW?
-;;
-;; A: I just generate an HTML string and slap it into the Hiccup tree.
-;;
-;;     (defn page [req]
-;;       (let [{:keys [slug]} (:params req)
-;;             page {:slug slug}
-;;             time-before-convert (now-ms)
-;;             page-html (->html page)
-;;             time-after-convert (now-ms)]
-;;         (page/html5
-;;          [:head (hiccup.page/include-css "/style.css")]
-;;          [:body
-;;           [:div {:class style/content-container}
-;;            [:a {:href "/"} "home"]
-;;            [:h2 slug]
-;;            [:pre (with-out-str (pprint/pprint (->meta page)))]
-;;            [:p "Inferred builder: " [:code  (pr-str (infer-builder page))]]
-;;            [:p "Page created in " [:em  (- time-after-convert time-before-convert)] " ms."]
-;;            [:div page-html]]])))
-;;
-;; Okay, let's to the same here.
-
-(let [repo-path ".." slug "olorm-1"]
-  [(str (fs/canonicalize repo-path) "/p/" slug)
-   (str
-    (fs/canonicalize
-     (fs/file repo-path "p" slug)))]
-  )
+;; ## View an olorm
 
 (def markdown->html
   (iki/cache-fn-by iki/markdown->html identity))
