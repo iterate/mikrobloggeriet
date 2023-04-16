324b93d524c1aa5756523868532fbc349892c326
Teodor Heggelund <git@teod.eu>
2023-04-16 22:44:50 +0200

    use :number, :olorm is deprecated

--- cli/src/olorm/cli.clj
+++ cli/src/olorm/cli.clj
@@ -52,7 +52,7 @@ your system, so we need to know where to find OLORM pages.
   (let [repo-path (repo-path)]
     (when-not (:disable-git-magic opts)
       (shell {:dir repo-path} "git pull --rebase"))
-    (let [next-number (inc (or (->> (olorm/olorms {:repo-path repo-path}) (map :olorm) sort last)
+    (let [next-number (inc (or (->> (olorm/olorms {:repo-path repo-path}) (map :number) sort last)
                               0))
           olorm (olorm/->olorm {:repo-path repo-path :number next-number})
           _ (prn olorm)
