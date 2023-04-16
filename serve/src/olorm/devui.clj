fd1cd274f3e25b0072834558a1392f33c3a6de40
Teodor Heggelund <git@teod.eu>
2023-04-16 14:21:38 +0200

    Trailing whitespace pls

--- serve/src/olorm/devui.clj
+++ serve/src/olorm/devui.clj
@@ -1,8 +1,12 @@
+;; OLORM DEVUI
+
 (ns olorm.devui
   (:require
    [nextjournal.clerk :as clerk]
    [olorm.lib :as olorm]
    [babashka.fs :as fs]))
+
+;; ## Session 1
 
 (defonce xx (atom nil))
 
@@ -30,6 +34,8 @@
   (map olorm/slug->olorm)
   (filter :olorm))
 
+;; ## Session 2
+
 (defn olorm->html [& _args])
 
 (olorm->html {:slug "olorm-1" :repo-path ".."})
@@ -38,3 +44,7 @@
 ;; not sure.
 ;; how do I want this to integrate with the rest of the system?
 ;; what am I doing (really) in OGGPOW?
+
+^
+{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/html [:div {:style {:height "50vh"}}])
