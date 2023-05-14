2ba770c13838499ebe2f64442fa1cb7cfe6bb9db
Teodor Heggelund <git@teod.eu>
2023-05-14 15:46:44 +0200

    Define a "mikrobloggeriet client"

--- lib/src/mikrobloggeriet/api.clj
+++ lib/src/mikrobloggeriet/api.clj
@@ -1,2 +1,9 @@
 (ns mikrobloggeriet.api)
 
+;; core concept: mikrobloggeriet client.
+;;
+;; Example:
+
+{:repo-path "/home/teodorlu/dev/iterate/olorm"
+ :document-dir "o"
+ :document-prefix "olorm-"}
