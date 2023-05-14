8842ae8aa70f61d8b50ad9010638ebd2983f9a7f
Teodor Heggelund <git@teod.eu>
2023-05-14 16:19:59 +0200

    Link to issue

--- lib/src/mikrobloggeriet/newdoc.clj
+++ lib/src/mikrobloggeriet/newdoc.clj
@@ -1,9 +1,12 @@
 ;; # Clerk repro
+;;
+;; This namespace reproduces a possible Clerk bug.
+;;
+;; Issue: https://github.com/nextjournal/clerk/issues/483
+
+{:nextjournal.clerk/visibility {:code :fold}}
 
 (ns mikrobloggeriet.newdoc
   {:nextjournal.clerk/toc true})
 
-{:nextjournal.clerk/visibility {:code :fold}}
-
 ;; ## First section
-
