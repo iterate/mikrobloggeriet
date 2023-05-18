7ab6fea18da5c01443afe1841c622a59153e7859
Teodor Heggelund <git@teod.eu>
2023-05-18 20:38:23 +0200

    Another deprecation warning!

--- olorm-cli/src/olorm/cli.clj
+++ olorm-cli/src/olorm/cli.clj
@@ -1,4 +1,6 @@
 (ns olorm.cli
+  ^{:deprecated true
+    :doc "Legacy OLORM CLI, please see README for updated installation instructions."}
   (:require
    [babashka.cli :as cli]
    [babashka.fs :as fs]
@@ -134,4 +136,13 @@ Example usage:
    {:cmds []                :fn olorm-help}])
 
 (defn -main [& args]
+  (let [deprecation-warning (str/trim "
+
+WARNING: You are running a legacy installation of the OLORM CLI (deprecated).
+Please follow instructions in README.md to install the new OLORM CLI.
+
+")]
+    (binding [*out* *err*]
+      (println deprecation-warning)
+      (println)))
   (cli/dispatch subcommands args))
