c727c3783c8b6fa223c89bc554af9b9265a866b6
Teodor Heggelund <git@teod.eu>
2023-07-16 17:08:58 +0200

    olorm.serve: tester

---
+++ serve/test/olorm/serve_test.clj
@@ -0,0 +1,19 @@
+(ns olorm.serve-test
+  (:require [olorm.serve :as serve]
+            [clojure.test :refer [is testing deftest] ]
+            [clojure.string :as str]))
+
+(deftest index-test
+  (let [index (serve/index {})]
+    (testing "An index was returned"
+      (is (some? index)))
+
+    (testing "Index looks like html"
+      (is (and (string? index)
+               (str/starts-with? index "<!DOCTYPE"))))
+
+    (testing "Index refers to olorm-4"
+      (is (str/includes? index "/o/olorm-4")))
+
+    (testing "olorm 4 html contains more than 500 chars"
+      (is (< 500 (count (:body (serve/olorm {:route-params {:slug "olorm-4"}}))))))))
