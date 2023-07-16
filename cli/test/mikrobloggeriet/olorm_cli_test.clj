4046af3f522054a0d75d007017331f5b0a992c4b
Teodor Heggelund <git@teod.eu>
2023-07-16 18:22:46 +0200

    Disable tests under CI

--- cli/test/mikrobloggeriet/olorm_cli_test.clj
+++ cli/test/mikrobloggeriet/olorm_cli_test.clj
@@ -8,28 +8,29 @@
   (testing "A repo path is set"
     (is (some? (olorm-cli/repo-path)))))
 
-(deftest create-opts->commands-test
-  (testing "we can generate commands without errors"
-    (is (some? (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git false :edit false}))))
+(when-not ci?
+  ;; none of these tests work under CI, because they assume a user has configured git user.name
+  (deftest create-opts->commands-test
+    (testing "we can generate commands without errors"
+      (is (some? (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git false :edit false}))))
 
-  (testing "A seq of commands is returned, the first element of a command is a keyword"
-    (let [commands (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git false :edit false})]
-      (doseq [c commands]
-        (is (keyword? (first c))))))
+    (testing "A seq of commands is returned, the first element of a command is a keyword"
+      (let [commands (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git false :edit false})]
+        (doseq [c commands]
+          (is (keyword? (first c))))))
 
-  (testing "When we transform to a dry run, only printable commands are returned"
-    (let [printable? (fn [command] (#{:prn :println} (first command)))
-          commands (map olorm-cli/command->dry-command (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git false :edit false}))]
-      (doseq [c commands]
-        (is (printable? c)))))
+    (testing "When we transform to a dry run, only printable commands are returned"
+      (let [printable? (fn [command] (#{:prn :println} (first command)))
+            commands (map olorm-cli/command->dry-command (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git false :edit false}))]
+        (doseq [c commands]
+          (is (printable? c)))))
 
-  (when-not ci? ; git is not configured on CI
     (testing "When git is enabled, there is shelling out"
       (let [commands (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git true :edit false})
             command-types (into #{} (map first commands))]
-        (is (contains? command-types :shell)))))
+        (is (contains? command-types :shell))))
 
-  (testing "When edit is enabled, there is shelling out"
-    (let [commands (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git false :edit true})
-          command-types (into #{} (map first commands))]
-      (is (contains? command-types :shell)))))
+    (testing "When edit is enabled, there is shelling out"
+      (let [commands (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git false :edit true})
+            command-types (into #{} (map first commands))]
+        (is (contains? command-types :shell))))))
