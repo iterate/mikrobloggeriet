edb6877c3c355b8d12fc66dfdf556dc8f5f23f71
Teodor Heggelund <git@teod.eu>
2023-07-16 17:53:04 +0200

    Kjør enda flere tester

--- serve/test/olorm/serve_test.clj
+++ serve/test/olorm/serve_test.clj
@@ -3,10 +3,6 @@
             [clojure.test :refer [is testing deftest] ]
             [clojure.string :as str]))
 
-(is true)
-
-#_
-
 (deftest index-test
   (let [index (serve/index {})]
     (testing "An index was returned"
