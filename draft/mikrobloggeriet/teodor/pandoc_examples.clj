37eaeefeaaa25d7f6c5f025ad8a3e66e2f1190d2
Teodor Heggelund <git@teod.eu>
2023-08-14 19:44:41 +0200

    Pandoc examples

---
+++ draft/mikrobloggeriet/teodor/pandoc_examples.clj
@@ -0,0 +1,6 @@
+(ns mikrobloggeriet.teodor.pandoc-examples
+  (:require [mikrobloggeriet.pandoc :as pandoc]))
+
+(pandoc/from-markdown "# olorm 11\n\nhei på deg")
+
+(pandoc/from-markdown "# olorm 11\n\nhei på deg")
