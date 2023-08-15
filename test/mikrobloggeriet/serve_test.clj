934508d222b3717e2b684a3c1bf25eb941984512
Olav Haaland Moseng <olav.moseng@gmail.com>
2023-08-15 16:27:43 +0200

    Remove: remove failing test to see how CI works. Actually this time...

--- test/mikrobloggeriet/serve_test.clj
+++ test/mikrobloggeriet/serve_test.clj
@@ -18,6 +18,3 @@
 
     (testing "olorm 4 html contains more than 500 chars"
       (is (< 500 (count (:body (serve/olorm {:route-params {:slug "olorm-4"}}))))))))
-
-(deftest olav-test
-  (is (= 1 2)))
