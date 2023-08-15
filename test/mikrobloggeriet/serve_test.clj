fe8b9cf242706c22be3f4a8aaeb71dd0173a99e4
Olav Haaland Moseng <olav.moseng@gmail.com>
2023-08-15 16:14:12 +0200

    Add: failing test to see how CI works

--- test/mikrobloggeriet/serve_test.clj
+++ test/mikrobloggeriet/serve_test.clj
@@ -18,3 +18,6 @@
 
     (testing "olorm 4 html contains more than 500 chars"
       (is (< 500 (count (:body (serve/olorm {:route-params {:slug "olorm-4"}}))))))))
+
+(deftest olav-test
+  (is true))
