7771e64860dc7969d546f6a42585c78e64d1e22c
Teodor Heggelund <git@teod.eu>
2023-08-16 13:40:28 +0200

    Var lookup: more stuff

--- draft/mikrobloggeriet/teodor/var_lookup.clj
+++ draft/mikrobloggeriet/teodor/var_lookup.clj
@@ -1,11 +1,12 @@
 (ns mikrobloggeriet.teodor.var-lookup
   (:require
+   [clojure.set :as set]
    [clojure.string :as str]))
 
 ;; I want to iterate over my namespaces and look for vars with certain metadata.
 
-(defn lookup-vars [ns-match? var-match?]
-  (->> (all-ns)
+(defn lookup-vars [namespaces ns-match? var-match?]
+  (->> namespaces
        (filter ns-match?)
        (mapcat ns-interns)
        (map val)
@@ -13,7 +14,38 @@
        (sort-by symbol)))
 
 ^{:nextjournal.clerk/auto-expand-results? true}
-(let [deprecated-vars (lookup-vars #(str/starts-with? % "clojure")
-                                   (comp :deprecated meta))]
-  [deprecated-vars
-   (meta (first deprecated-vars))])
+(lookup-vars (all-ns)
+             #(str/starts-with? % "clojure")
+             (comp :deprecated meta))
+
+^{:nextjournal.clerk/auto-expand-results? true}
+(lookup-vars (all-ns)
+             #(str/starts-with? % "clojure")
+             (comp :deprecated meta))
+
+#_
+(let [allns-set (into #{} (map symbol (all-ns)))
+      loadedlibs-set (into #{} (loaded-libs))]
+  (count (set/intersection allns-set loadedlibs-set)))
+
+(defn allns-syms []
+  (map (comp symbol ns-name)
+       (all-ns)))
+
+^{:nextjournal.clerk/auto-expand-results? true}
+(take 10 (sort (allns-syms)))
+
+^{:nextjournal.clerk/auto-expand-results? true}
+(take 10 (sort (loaded-libs)))
+
+(count (all-ns))
+(count (loaded-libs))
+
+
+(str/starts-with? 'clojure "clojure")
+
+(->> (loaded-libs)
+     (filter #(str/starts-with? % "java")))
+
+(->> (allns-syms)
+     (filter #(str/starts-with? % "java")))
