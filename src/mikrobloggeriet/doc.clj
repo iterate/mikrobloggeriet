de83a53781ec2c839c7e41a1c7b126d4867bd2dd
Teodor Heggelund <git@teod.eu>
2023-08-19 14:29:49 +0200

    doc/href: prefer nil punning
    Don't raise assertions, just return nil if input is stupid

--- src/mikrobloggeriet/doc.clj
+++ src/mikrobloggeriet/doc.clj
@@ -5,9 +5,9 @@
 (defn href
   "Create a link to a doc in a cohort"
   [cohort doc]
-  (assert (:cohort/id cohort))
-  (assert (:doc/slug doc))
-  (str "/" (name (:cohort/id cohort)) "/" (:doc/slug doc)))
+  (when (and (:cohort/id cohort)
+             (:doc/slug doc))
+    (str "/" (name (:cohort/id cohort)) "/" (:doc/slug doc))))
 
 (defn exists? [cohort doc]
   (and (:cohort/root cohort)
