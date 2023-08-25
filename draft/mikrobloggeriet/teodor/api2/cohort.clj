f428e590b263646a87b73a8128874a6d5db34701
Teodor Heggelund <git@teod.eu>
2023-08-25 17:58:19 +0200

    Proposal: store, cohort and doc
    Untill now, we have struggled to figure out whether functionality should
    live in `mikrobloggeriet.cohort` or `mikrobloggeriet.doc`. A symptom of
    this is cyclic dependencies. Does doc depend on cohort? Does cohort
    depend on doc?
    
    We propose to break this dependency cycle by keeping the abstractions
    for cohort and doc narrow, and introducing a _store_. The store knows
    about cohorts and documents.

---
+++ draft/mikrobloggeriet/teodor/api2/cohort.clj
@@ -0,0 +1,20 @@
+(ns mikrobloggeriet.teodor.api2.cohort
+  "A cohort is a set of people writing toghether")
+
+(comment
+  ;; example cohort:
+  (sorted-map
+   :cohort/root "text/oj"
+   :cohort/slug "oj"
+   :cohort/members [{:author/first-name "Johan"}
+                    {:author/first-name "Olav"}])
+  )
+
+;; This namespace contains no constructors.
+;; See store.clj for available cohorts.
+
+(defn slug [cohort]
+  (:cohort/slug cohort))
+
+(defn root [cohort]
+  (:cohort/root cohort))
