12522721449d5a7ba178b63b0d13ab0dd180b313
Teodor Heggelund <git@teod.eu>
2023-08-25 13:31:22 +0200

    Every command is a keyword

--- test/mikrobloggeriet/cli_test.clj
+++ test/mikrobloggeriet/cli_test.clj
@@ -2,9 +2,43 @@
   (:require [mikrobloggeriet.cli :as cli]
             [clojure.test :refer [deftest testing is]]))
 
+(comment
+  (deftest example-test
+    (testing "we can generate commands without errors"
+      (is (some? (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git false :edit false}))))
+
+    (testing "A seq of commands is returned, all commands are prints"
+      (is (every? keyword?
+                  (->> (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git false :edit false})
+                       (map first)))))
+
+    (testing "When we transform to a dry run, only printable commands are returned"
+      (is (every? #{:prn :println}
+                  (->> (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path)
+                                                         :git false
+                                                         :edit false})
+                       (map olorm-cli/command->dry-command)
+                       (map first)))))
+
+    (testing "When git is enabled, there is shelling out"
+      (is (contains? (->> (olorm-cli/create-opts->commands {:dir (olorm-cli/repo-path) :git true :edit false})
+                          (map first)
+                          (into #{}))
+                     :shell))))
+
+  )
+
 (deftest create-opts->commands-test
   (testing "we can generate commands without errors"
-    (is (some? (cli/create-opts->commands {:dir "." :git false :edit false})))))
+    (is (some? (cli/create-opts->commands {:dir "." :git false :edit false}))))
+
+  (testing "A seq of commands is returned, all commands are prints"
+    (is (every? keyword?
+                (->> (cli/create-opts->commands {:dir "." :git false :edit false})
+                     (map first)))))
+  )
+
+
 
 (deftest learn-test
   (testing "some? returns true for all non-nil values"
