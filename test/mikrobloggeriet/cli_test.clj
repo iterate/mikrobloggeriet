dbb78f8d8488cde9166947eae395967c181a9a56
Teodor Heggelund <git@teod.eu>
2023-08-25 13:07:00 +0200

    some true for non-nil values

--- test/mikrobloggeriet/cli_test.clj
+++ test/mikrobloggeriet/cli_test.clj
@@ -2,8 +2,16 @@
   (:require [mikrobloggeriet.cli :as cli]
             [clojure.test :refer [deftest testing is]]))
 
-(deftest create-opts->commands-test
+(deftest name-test
   (testing "no directory"
     (is (nil? (cli/create-opts->commands {:dir ""
                                           :git ""
                                           :edit ""})))))
+
+(deftest learn-test
+  (testing "some? returns true for all non-nil values"
+    (is (some? 42))
+    (is (some? false))
+    (is (not (some? nil)))
+    )
+  )
