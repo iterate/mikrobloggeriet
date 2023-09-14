52b00c2b224cd52f7c43e6de972e1c24d8cb5695
Teodor Heggelund <git@teod.eu>
2023-09-14 10:31:51 +0200

    Refactor: don't use deprecated code
    Replace test for deprecated code with test for non-deprecated code

--- test/mikrobloggeriet/doc_test.clj
+++ test/mikrobloggeriet/doc_test.clj
@@ -1,13 +1,7 @@
 (ns mikrobloggeriet.doc-test
   (:require
    [clojure.test :refer [deftest is]]
-   [mikrobloggeriet.cohort :as cohort]
    [mikrobloggeriet.doc :as doc]))
-
-(deftest index-md-path-test
-  (let [md-file (doc/index-md-path cohort/olorm {:doc/slug "olorm-1"})]
-    (is (= "o/olorm-1/index.md"
-           (str md-file)))))
 
 (deftest number-test
   (is (= 1 (doc/number {:doc/slug "olorm-1"})))
--- test/mikrobloggeriet/store_test.clj
+++ test/mikrobloggeriet/store_test.clj
@@ -2,7 +2,8 @@
   (:require
    [clojure.test :refer [deftest is]]
    [mikrobloggeriet.store :as store]
-   [mikrobloggeriet.doc :as doc]))
+   [mikrobloggeriet.doc :as doc]
+   [mikrobloggeriet.cohort :as cohort]))
 
 (deftest doc-exists?-test
   (is (store/doc-exists? store/olorm
@@ -11,3 +12,18 @@
 (deftest doc-href-test
   (is (= "/olorm/olorm-1/"
          (store/doc-href store/olorm (doc/from-slug "olorm-1")))))
+
+
+
+(store/doc-md-path store/olorm (doc/from-slug "olorm-1"))
+
+(store/doc-md-path (cohort/set-repo-path store/olorm "/TESTROOT")
+                   (doc/from-slug "olorm-1"))
+
+(deftest doc-md-path-test
+  (let [cohort (cohort/set-repo-path store/olorm "/TESTFOLDER")]
+    (is (= (str "/TESTFOLDER/"
+                (cohort/root cohort)
+                "/olorm-1/index.md")
+           (str (store/doc-md-path (cohort/set-repo-path store/olorm "/TESTFOLDER")
+                                   (doc/from-slug "olorm-1")))))))
