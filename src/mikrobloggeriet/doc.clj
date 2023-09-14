d391251ac137dfd917930347954af6f176f81ee5
Teodor Heggelund <git@teod.eu>
2023-09-14 11:03:12 +0200

    Delete dead code

--- src/mikrobloggeriet/doc.clj
+++ src/mikrobloggeriet/doc.clj
@@ -1,18 +1,6 @@
 (ns mikrobloggeriet.doc
   (:require
-   [babashka.fs :as fs]
    [clojure.string :as str]))
-
-;; I wonder if we should change all these function signatures from
-;;
-;;     (fn [cohort doc] ,,,)
-;;
-;; to
-;;
-;;     (fn [doc cohort] ,,,)
-;;
-;; in order to take the object first.
-;; I think this will help make threading easier.
 
 (defn from-slug [slug]
   {:doc/slug slug})
@@ -23,32 +11,3 @@
 (defn number [doc]
   (when-let [slug (:doc/slug doc)]
     (parse-long (last (str/split slug #"-")))))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; DEPRECATED FUNCTIONS
-;;
-;; Do not use!
-
-(defn
-  ^:deprecated
-  exists? [cohort doc]
-  (and (:cohort/root cohort)
-       (:doc/slug doc)
-       (fs/directory? (fs/file (:cohort/root cohort)
-                               (:doc/slug doc)))
-       (fs/exists? (fs/file (:cohort/root cohort)
-                            (:doc/slug doc)
-                            "meta.edn"))
-       (fs/exists? (fs/file (:cohort/root cohort)
-                            (:doc/slug doc)
-                            "index.md"))))
-
-(defn
- ^:deprecated
-  href
-  "Create a link to a doc in a cohort"
-  [cohort doc]
-  (when (and (:cohort/id cohort)
-             (:doc/slug doc))
-    (str "/" (name (:cohort/id cohort)) "/" (:doc/slug doc))))
-
