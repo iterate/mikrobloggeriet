b03c07fe406cab4a03152243e4fb28154feb5368
Teodor Heggelund <git@teod.eu>
2023-12-06 08:57:13 +0100

    Teodor julekalender: hva vil jeg skrive om?

---
+++ draft/mikrobloggeriet/teodor/julekalender_2023_tema.clj
@@ -0,0 +1,13 @@
+(ns mikrobloggeriet.teodor.julekalender-2023-tema)
+
+;; hva vil jeg skrive om?
+;;
+;; 1. Moldable development / interactive development
+;;    Du må skrive kode for å kjøre koden din!
+;;    Du må ha hypoteser!
+;;
+;; 2. Debugging with the scientific method.
+;;    Du må ha en hypotese.
+;;      Hvis du ikke har en hypotese, er steg 1 å lage en hypotese!
+;;      Sette seg ned foran PC-en og begynne å skrive funker ikke, i hvertfall ikke for meg.
+;;    Du må designe et eksperiment.
