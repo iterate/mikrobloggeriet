1a622863ecadf723a665ea5f59a28efc3c139330
Teodor Heggelund <git@teod.eu>
2023-12-12 12:27:51 +0100

    Kode er en designdisiplin

--- draft/mikrobloggeriet/teodor/julekalender_2023_tema.clj
+++ draft/mikrobloggeriet/teodor/julekalender_2023_tema.clj
@@ -1,18 +1,29 @@
 (ns mikrobloggeriet.teodor.julekalender-2023-tema)
 
-;; hva vil jeg skrive om?
-;;
-;; 1. Moldable development / interactive development
-;;    Du må skrive kode for å kjøre koden din!
-;;    Du må ha hypoteser!
-;;
-;; 2. Debugging with the scientific method.
-;;    Du må ha en hypotese.
-;;      Hvis du ikke har en hypotese, er steg 1 å lage en hypotese!
-;;      Sette seg ned foran PC-en og begynne å skrive funker ikke, i hvertfall ikke for meg.
-;;    Du må designe et eksperiment.
-;;
-;; 3. Observability.
-;;    Ofte brukt i distribuerte systemer.
-;;    Like relevant lokalt!
-;;    Mer enn logging.
+(defmacro option [name & body])
+
+(option "Moldable development / interactive development"
+        ;;    Du må skrive kode for å kjøre koden din!
+        ;;    Du må ha hypoteser!
+        )
+
+(option "Debugging with the scientific method"
+        ;;    Du må ha en hypotese.
+        ;;      Hvis du ikke har en hypotese, er steg 1 å lage en hypotese!
+        ;;      Sette seg ned foran PC-en og begynne å skrive funker ikke, i hvertfall ikke for meg.
+        ;;    Du må designe et eksperiment.
+        )
+
+(option "Observability"
+        ;;    Ofte brukt i distribuerte systemer.
+        ;;    Like relevant lokalt!
+        ;;    Mer enn logging.
+        )
+
+(option "Koding er en designdisiplin"
+        ;; organisert under vitenskap og matematikk på universiteter
+        ;; kode /gjør noe/
+        ;; gjør /hva?/
+        ;;
+        ;; Systemdesign
+        )
