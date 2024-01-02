7721628698fd7be21e50d62ea0987e758cf6bc33
Olav Haaland Moseng <42978548+olavm@users.noreply.github.com>
2024-01-02 14:14:43 +0100

    Clerk setup to create md links for authors of luke

--- draft/mikrobloggeriet/olav/olav_olav.clj
+++ draft/mikrobloggeriet/olav/olav_olav.clj
@@ -1,12 +1,33 @@
-(ns mikrobloggeriet.olav.olav-olav 
-  (:require [mikrobloggeriet.olorm :as olorm]
-            [mikrobloggeriet.pandoc]
-            [nextjournal.clerk :as clerk]))
+(ns mikrobloggeriet.olav.olav-olav
+  (:require [nextjournal.clerk :as clerk]
+            [mikrobloggeriet.store :as store]
+            [mikrobloggeriet.serve :as serve]
+            [clojure.string :as str]))
 
-(+ 1 2)
+(let [cohort store/luke]
+  (clerk/table (map (fn [doc]
+                      (merge doc
+                             {:title (:title (serve/markdown->html+info (slurp (store/doc-md-path cohort doc))))
+                              :author (store/author-first-name cohort doc)
+                              :href (store/doc-href cohort doc)
+                              :html (clerk/html [:a {:href (store/doc-href cohort doc)} (store/author-first-name cohort doc)])}))
+                    (store/docs cohort))))
 
-(def olorms
-  "List of all published olorms"
-  (mikrobloggeriet.olorm/docs {:repo-path "."}))
+(defn cohort-authors-href [cohort]
+  (map (fn [doc]
+         {:author (store/author-first-name cohort doc)
+          :href (store/doc-href cohort doc)})
+       (store/docs cohort)))
 
-;; (clerk/html (iki.api/markdown->html (slurp (olorm/index-md-path (nth olorms 5)))))
+
+(defn markdown-links [title-fn href-fn elements]
+  (str/join ", " (map (fn [x]
+                        (str "[" (title-fn x) "]" "(" (href-fn x) ")"))
+                      elements)))
+
+(let [md-links (markdown-links :author :href (cohort-authors-href store/luke))]
+  (clerk/fragment (clerk/md md-links)
+                  (clerk/html [:code md-links])))
+
+(comment
+  (clerk/clear-cache!))
