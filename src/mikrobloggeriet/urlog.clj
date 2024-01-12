99c43c0ca2c994151e8af691c7ef94371470ef3a
Olav Haaland Moseng <42978548+olavm@users.noreply.github.com>
2024-01-12 18:03:52 +0100

    Move code in for to a let

--- src/mikrobloggeriet/urlog.clj
+++ src/mikrobloggeriet/urlog.clj
@@ -2,7 +2,8 @@
   (:require
    [hiccup.page :as page]
    [clojure.edn :as edn]
-   [babashka.fs :as fs]))
+   [babashka.fs :as fs]
+   [mikrobloggeriet.urlog :as urlog]))
 
 (def urlogfile-path "text/urlog/urls.edn")
 (def doors-dir "src/mikrobloggeriet/urlog_assets/doors/")
@@ -23,8 +24,6 @@
 (defn logo []
   [:pre {:class :logo}
    (slurp "src/mikrobloggeriet/urlog_assets/logo.txt")])
-
-
 
 (defn door-path+url->html [door-path url]
   [:div {:class :wall}
@@ -53,7 +52,8 @@
      [:p {:class "intro"}
       "Tilfeldige dører til internettsteder som kan være morsomme og/eller interessante å besøke en eller annen gang."]]
     [:div {:class "all-doors"}
-     (for [doc (reverse (:urlog/docs (edn/read-string (slurp urlogfile-path))))]
-       (let [door-path (rand-nth (door-paths))
-             url (:urlog/url doc)]
-         (door-path+url->html door-path url)))]]))
+     (let [urlog-data (edn/read-string (slurp urlogfile-path))]
+       (for [doc (reverse (:urlog/docs urlog-data))]
+         (let [door-path (rand-nth (door-paths))
+               url (:urlog/url doc)]
+           (door-path+url->html door-path url))))]]))
