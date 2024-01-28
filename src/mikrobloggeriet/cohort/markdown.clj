d2563006becbc29892412acaa3b83fa5d4d4b5de
Teodor Heggelund <git@teod.eu>
2024-01-28 16:26:28 +0100

    Revert "markdown cohort and URL cohort"
    This reverts commit 71e7f2df89157f70429def33c24ec88ff76859ad.

--- src/mikrobloggeriet/cohort/markdown.clj
+++
@@ -1,3 +0,0 @@
-(ns mikrobloggeriet.cohort.markdown)
-
-;; a markdown cohort is a cohort in which we write text using markdown together.
--- src/mikrobloggeriet/cohort/url.clj
+++
@@ -1,5 +0,0 @@
-(ns ^{:codeowners [{:url "https://github.com/nenomindjek"}
-                   {:url "https://github.com/olavm"}]}
-    mikrobloggeriet.cohort.url)
-
-;; an URL cohort is a cohort in which we share URLs together.
