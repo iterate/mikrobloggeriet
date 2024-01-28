71e7f2df89157f70429def33c24ec88ff76859ad
Teodor Heggelund <git@teod.eu>
2024-01-28 12:09:36 +0100

    markdown cohort and URL cohort

---
+++ src/mikrobloggeriet/cohort/markdown.clj
@@ -0,0 +1,3 @@
+(ns mikrobloggeriet.cohort.markdown)
+
+;; a markdown cohort is a cohort in which we write text using markdown together.
---
+++ src/mikrobloggeriet/cohort/url.clj
@@ -0,0 +1,5 @@
+(ns ^{:codeowners [{:url "https://github.com/nenomindjek"}
+                   {:url "https://github.com/olavm"}]}
+    mikrobloggeriet.cohort.url)
+
+;; an URL cohort is a cohort in which we share URLs together.
