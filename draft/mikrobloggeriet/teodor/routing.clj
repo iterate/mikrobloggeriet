7e59b39519830b9153e91697facea0b763d7606f
Teodor Heggelund <git@teod.eu>
2024-01-30 21:54:06 +0100

    explore some routing

---
+++ draft/mikrobloggeriet/teodor/routing.clj
@@ -0,0 +1,16 @@
+(ns mikrobloggeriet.teodor.routing
+  (:require
+   [reitit.core :as reitit]))
+
+(let [router (reitit/router [["/offers/" {:get (fn [_req] "our offers!")}]])]
+  (reitit/match-by-path router "/offers/"))
+;; => {:template "/offers/",
+;;     :data {:get #function[mikrobloggeriet.teodor.routing/eval30688/fn--30689]},
+;;     :result nil,
+;;     :path-params {},
+;;     :path "/offers/"}
+
+(let [router (reitit/router [["/offers/" {:get (fn [_req] "our offers!")}]])]
+
+  #_
+  (reitit/match-by-path router "/offers/"))
