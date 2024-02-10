50854c25112695af5a6fc9f35efa1c59b666cf95
Teodor Heggelund <git@teod.eu>
2024-02-10 16:44:22 +0100

    connect to local pg with psql

--- draft/mikrobloggeriet/teodor/db.clj
+++ draft/mikrobloggeriet/teodor/db.clj
@@ -1,7 +1,10 @@
 ;; # Database for Mikrobloggeriet?
 
 (ns mikrobloggeriet.teodor.db
-  )
+  (:require
+   [pg.core :as pg]))
+
+
 
 ;; skal vi ha tilstand?
 ;; Det har fordeler :)
@@ -14,3 +17,21 @@
 ;; - next.jdbc er flott
 ;; - men jeg har lyst til å kunne jobbe mot /rå postgres/.
 ;;   Ikke bare gjøre "generell SQL".
+
+#_
+(def
+  "Does not yet work!"
+  config
+  {:host "127.0.0.1"
+   :port 10140
+   :user "test"
+   :password "test"
+   :database "test"})
+
+#_
+(defonce conn (pg/connect config))
+
+(comment
+
+
+  )
