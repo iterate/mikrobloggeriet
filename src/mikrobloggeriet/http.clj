404404202b3e98649a8e4c67a1bea021cbd83738
Teodor Heggelund <git@teod.eu>
2024-02-11 00:36:46 +0100

    Delete Compojure leftovers

--- src/mikrobloggeriet/http.clj
+++ src/mikrobloggeriet/http.clj
@@ -1,24 +1,16 @@
 (ns mikrobloggeriet.http)
 
 (defn permanent-redirect
-  "Permanent redirect to target.
-
-  The :body \"\" part is kept to ease over a difference between Compojure and
-  Reitit during the router migration of 2024-01."
+  "Permanent redirect to target"
   [{:keys [target]}]
   (assert target)
   {:status 308
-   :headers {"Location" target}
-   :body ""})
+   :headers {"Location" target}})
 
 (defn response-ok? [response]
   (= 200 (:status response)))
 
 (defn path-param
-  "Get a path parameter from an HTTP request.
-
-  Supports Compojure style parameters and Reitit style parameters."
+  "Get a path parameter from an HTTP request."
   [req param]
-  (or (get-in req [:route-params param]) ;; compojure
-      (get-in req [:path-params param]) ;; reitit
-      ))
+  (get-in req [:path-params param]))
