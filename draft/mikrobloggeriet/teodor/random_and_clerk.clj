bbdd120695252aa87e58ff76b25e25517feb1875
Teodor Heggelund <git@teod.eu>
2024-02-10 19:15:28 +0100

    Eksempel: tilfeldighet i Clerk

---
+++ draft/mikrobloggeriet/teodor/random_and_clerk.clj
@@ -0,0 +1,12 @@
+(ns mikrobloggeriet.teodor.random-and-clerk)
+
+;; Olav: vi snakket såvidt om caching i Clerk.
+;; nextjournal.clerk/clear-cache! sletter _hele_ cachen.
+;;
+;; Hvis du i stedet setter :nextjournal.clerk/no-cache til true i
+;; metadataen (foran et toppnivå-uttrykk) vil Clerk alltid regne ut uttrykket.
+;;
+;; Eksempel:
+
+^:nextjournal.clerk/no-cache
+(rand)
