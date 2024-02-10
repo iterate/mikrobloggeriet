d9947a899c895625a65bb39e69092a4587587fd1
Teodor Heggelund <git@teod.eu>
2024-02-10 16:31:30 +0100

    tenk DB

--- draft/mikrobloggeriet/teodor/db.clj
+++ draft/mikrobloggeriet/teodor/db.clj
@@ -1,6 +1,16 @@
-(ns mikrobloggeriet.teodor.db)
+;; # Database for Mikrobloggeriet?
+
+(ns mikrobloggeriet.teodor.db
+  )
 
 ;; skal vi ha tilstand?
 ;; Det har fordeler :)
 
 ;; la oss prøve litt.
+
+;; Vi går med https://github.com/igrishaev/pg2.
+;; Hvorfor?
+;;
+;; - next.jdbc er flott
+;; - men jeg har lyst til å kunne jobbe mot /rå postgres/.
+;;   Ikke bare gjøre "generell SQL".
