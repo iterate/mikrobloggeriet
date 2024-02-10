5aa533e848778f0bbd9d26fb682290687db4e079
Teodor Heggelund <git@teod.eu>
2024-02-10 17:49:46 +0100

    HOPS env vars for PostgreSQL

---
+++ src/mikrobloggeriet/db.clj
@@ -0,0 +1,40 @@
+(ns mikrobloggeriet.db
+  (:require
+   [mikrobloggeriet.config :as config]))
+
+(def dev-config
+  {:host "localhost"
+   :port config/pg-port
+   :user "mikrobloggeriet"
+   :password "mikrobloggeriet"
+   :database "mikrobloggeriet"})
+
+;; igrishaev/pg2 config docs: https://github.com/igrishaev/pg2?tab=readme-ov-file#connecting-to-the-server
+;;
+;; HOPS PG env vars: https://hops-doc.app.iterate.no/reference/environment_variables.html
+
+(def hops->pg2
+  {"PGDATABASE" :database
+   "PGHOST" :host
+   "PGPASSWORD" :password
+   "PGPORT" :port
+   "PGUSER" :user})
+
+(def pg2->hops
+  (into {}
+        (for [[k v] hops->pg2]
+          [v k])))
+
+(defn hops-config [env]
+  (into {}
+        (for [[envvar confkey] hops->pg2]
+          [confkey (get env envvar)])))
+
+(comment
+  (hops-config {"PGDATABASE" "mikrobloggeriet"
+                "PGHOST" "localhost"
+                "PGPASSWORD" "mikrobloggeriet"
+                "PGPORT" config/pg-port
+                "PGUSER" "mikrobloggeriet"})
+
+  (System/getenv))
