5c93b0edd61fd6004b553204e75c0f967b972426
Teodor Heggelund <git@teod.eu>
2024-02-11 17:10:13 +0100

    Migrations for dev and prod

--- src/mikrobloggeriet/db/migrate.clj
+++ src/mikrobloggeriet/db/migrate.clj
@@ -5,6 +5,7 @@
    [nextjournal.clerk :as clerk]
    [pg.core :as pg]
    [ragtime.core :as ragtime]
+   [ragtime.strategy]
    [ragtime.protocols]))
 
 (defn ensure-migrations-table!
@@ -20,9 +21,9 @@ create table if not exists migrations(
   (assert (not (System/getenv "HOPS_ENV")) "Do NOT drop the migrations table in production.")
   (pg/query conn "drop table migrations"))
 
-(defonce dev-conn (:mikrobloggeriet.system/db @repl/state))
 
 (comment
+  (defonce dev-conn (:mikrobloggeriet.system/db @repl/state))
   (alter-var-root #'dev-conn (constantly (:mikrobloggeriet.system/db @repl/state)))
   (ensure-migrations-table! dev-conn)
   (dangerously-delete-migrations-table!! dev-conn)
@@ -41,23 +42,48 @@ create table if not exists migrations(
     (->> (pg/query conn "select id from migrations")
          (map :id))))
 
-(def migrations [(reify ragtime.protocols/Migration
-                   (id [_] "add-foo-table")
-                   (run-up! [_ db] (pg/query (.conn db) "create table foo (id integer primary key, description text)"))
-                   (run-down! [_ db] (pg/query (.conn db) "create table foo (id integer primary key, description text)")))])
+(defn sql-migration [{:keys [id up down]}]
+  (reify ragtime.protocols/Migration
+    (id [_] id)
+    (run-up! [_ db] (pg/query (.conn db) up))
+    (run-down! [_ db] (pg/query (.conn db) down))))
+
+
+(def all-migrations
+  [(sql-migration {:id "add-foo-table"
+                   :up "create table foo (id integer primary key, description text)"
+                   :down "create table foo (id integer primary key, description text)"})
+   (sql-migration {:id "add-bar-table"
+                   :up "create table bar (id integer)"
+                   :down "drop table bar"})])
+
+(def migration-index (ragtime/into-index all-migrations))
+
+(def migrations
+  [(sql-migration {:id "add-foo-table"
+                   :up "create table foo (id integer primary key, description text)"
+                   :down "create table foo (id integer primary key, description text)"})])
 
 (comment
-  (ragtime/migrate (PgDatabase. dev-conn)
-                   (first migrations))
+  (defonce dev-conn (:mikrobloggeriet.system/db @repl/state))
 
-  (let [index (ragtime/into-index migrations)]
-    (ragtime/applied-migrations (PgDatabase. dev-conn) index))
+  ;; in production, migrate with ragtime.strategy/raise-error
+  (ragtime/migrate-all (PgDatabase. dev-conn)
+                       migration-index
+                       migrations
+                       {:strategy ragtime.strategy/raise-error})
 
+  ;; in development, migrate with ragtime.strategy/rebase
+  (ragtime/migrate-all (PgDatabase. dev-conn)
+                       migration-index
+                       migrations
+                       {:strategy ragtime.strategy/rebase})
 
+  ^::clerk/no-cache
+  (clerk/table
+   (pg/query dev-conn "select * from migrations"))
   )
 
-^::clerk/no-cache
-(pg/query dev-conn "select * from migrations")
 
 ^{:nextjournal.clerk/visibility {:code :hide}}
 (clerk/html [:div {:style {:height "50vh"}}])
