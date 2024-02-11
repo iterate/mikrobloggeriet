e75dc1f0bcaad9c06c0056ea5f9ff23ea3e90c23
Teodor Heggelund <git@teod.eu>
2024-02-11 19:50:32 +0100

    Access logs

--- draft/mikrobloggeriet/teodor/traffic.clj
+++ draft/mikrobloggeriet/teodor/traffic.clj
@@ -1,8 +1,15 @@
 (ns mikrobloggeriet.teodor.traffic
   (:require
    [mikrobloggeriet.repl :as repl]
+   [nextjournal.clerk :as clerk]
    [pg.core :as pg]))
 
 (defonce conn (:mikrobloggeriet.system/db @repl/state))
 
-(pg/query conn "select * from access_logs")
+(comment
+  (alter-var-root #'conn (constantly (:mikrobloggeriet.system/db @repl/state)))
+
+  )
+
+(clerk/table
+ (pg/query conn "select * from access_logs"))
