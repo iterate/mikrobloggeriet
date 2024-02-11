bd48344c2b6c979abee8d0f1eaa5f097bd4cfb47
Teodor Heggelund <git@teod.eu>
2024-02-11 14:38:55 +0100

    Work with a live database

--- src/mikrobloggeriet/db/migrate.clj
+++ src/mikrobloggeriet/db/migrate.clj
@@ -1,5 +1,8 @@
 (ns mikrobloggeriet.db.migrate
-  "Database migrations on Mikrobloggeriet")
+  "Database migrations on Mikrobloggeriet"
+  (:require
+   [mikrobloggeriet.repl :as repl]
+   [pg.core :as pg]))
 
 (defn ensure-migrations-table!
   [conn]
@@ -7,3 +10,12 @@
 
 (defn delete-migrations-table!
   [conn])
+
+(defonce dev-conn (:mikrobloggeriet.system/db @repl/state))
+
+(comment
+  (alter-var-root #'dev-conn (constantly (:mikrobloggeriet.system/db @repl/state)))
+
+  )
+
+(pg/query dev-conn "select 123 as num")
