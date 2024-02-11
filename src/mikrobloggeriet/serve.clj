602649f77372acd92276f8fd4f5f78e312febf30
Teodor Heggelund <git@teod.eu>
2024-02-11 19:49:03 +0100

    Start collecting access logs

--- src/mikrobloggeriet/serve.clj
+++ src/mikrobloggeriet/serve.clj
@@ -325,14 +325,12 @@
    ["/:slug/" {:get (fn [req] (doc req cohort))}] ])
 
 (defn before-app [req]
-  (when-let [conn (:mikrobloggeriet.system/db req)]
-    (tap> req)
-    (let [{:keys [uri request-method]} req]
-      #_
-      (pg/execute conn
-                  "insert into access_logs(method, uri) values ($1, $2)"
-                  {:params [(name request-method) uri]}))
-    (tap> ::success))
+  (future
+    (when-let [conn (:mikrobloggeriet.system/db req)]
+      (let [{:keys [uri request-method]} req]
+        (pg/execute conn
+                    "insert into access_logs(method, uri) values ($1, $2)"
+                    {:params [(name request-method) uri]}))))
   req)
 
 (defn app
