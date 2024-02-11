230ed7ab724c35c8317da861ecfb6aa04582a8bb
Teodor Heggelund <git@teod.eu>
2024-02-11 17:56:46 +0100

    Config: attach real URLS to ports

--- src/mikrobloggeriet/config.clj
+++ src/mikrobloggeriet/config.clj
@@ -1,5 +1,9 @@
 (ns mikrobloggeriet.config)
 
+;; HTTP server: http://localhost:7223
+;; Clerk:       http://localhost:7743
+;; PostgreSQL:  http://localhost:7574 (not sure if http?)
+
 (def http-server-port 7223)
 (def clerk-port 7743)
 (def pg-port 7574)
