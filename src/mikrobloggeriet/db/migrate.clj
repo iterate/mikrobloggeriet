c0f6f4bd31d5ef49e9ef970ee1bca4dff2e22459
Teodor Heggelund <git@teod.eu>
2024-02-11 14:44:33 +0100

    db.migrate: work with Clerk & REPL

--- src/mikrobloggeriet/db/migrate.clj
+++ src/mikrobloggeriet/db/migrate.clj
@@ -2,7 +2,8 @@
   "Database migrations on Mikrobloggeriet"
   (:require
    [mikrobloggeriet.repl :as repl]
-   [pg.core :as pg]))
+   [pg.core :as pg]
+   [nextjournal.clerk :as clerk]))
 
 (defn ensure-migrations-table!
   [conn]
@@ -19,3 +20,17 @@
   )
 
 (pg/query dev-conn "select 123 as num")
+
+(pg/query dev-conn "create table if not exists foo(id integer not null, description text)")
+
+(comment
+  (pg/execute dev-conn "insert into foo(id, description) values ($1, $2)" {:params [1 "first"]})
+
+
+  )
+
+^::clerk/no-cache
+(clerk/table (pg/query dev-conn "select * from foo"))
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/html [:div {:style {:height "50vh"}}])
