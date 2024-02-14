6a8f3c64a6c8a0cb45b2b1ee8e6b067ea73cb32e
Teodor Heggelund <git@teod.eu>
2024-02-14 15:10:04 +0100

    Rewrite Copy
    Hilsen Teodor og Elisabeth :)

--- src/mikrobloggeriet/serve.clj
+++ src/mikrobloggeriet/serve.clj
@@ -189,7 +189,7 @@
         [:h2 "Hva er dette for noe?"]
         [:p
          "Mikrobloggeriet er et initiativ der folk fra " [:a {:href iterate-url} "Iterate"] " deler ting de bryr seg om i hverdagen. "
-         "Vi velger å publisere fritt tilgjengelig på Internett fordi vi har tro på å dele kunnskap. "
+         "Vi publiserer fritt tilgjengelig på Internett fordi vi har tro på å dele kunnskap. "
          "Innhold og kode for Mikrobloggeriet på " [:a {:href github-mikrobloggeriet-url} "github.com/iterate/mikrobloggeriet"] ". "
          "Mikrobloggeriet kjører på " [:a {:href hops-url} "Headless Operations"] ". "
          "Hvis du har spørsmål eller kommentarer, kan du ta kontakt med " [:a {:href teodor-url} "Teodor"] "."]]
