6f02277a0e2678b0744de65b3656240a20a43722
Teodor Heggelund <git@teod.eu>
2024-03-02 19:34:21 +0100

    Access logs: filter per cohort

--- draft/mikrobloggeriet/teodor/try_db.clj
+++ draft/mikrobloggeriet/teodor/try_db.clj
@@ -3,7 +3,10 @@
 (ns mikrobloggeriet.teodor.try-db
   (:require
    [mikrobloggeriet.config :as config]
-   [pg.core :as pg]))
+   [nextjournal.clerk :as clerk]
+   [pg.core :as pg]
+   [mikrobloggeriet.store :as store]
+   [clojure.string :as str]))
 
 ;; skal vi ha tilstand?
 ;; Det har fordeler :)
@@ -27,3 +30,21 @@
 (defonce conn (pg/connect config))
 
 (pg/query conn "select 1 as one")
+
+(defn uri->cohort-slug [uri]
+  (-> uri
+      (str/split #"/")
+      second))
+
+(def cohort-slugs
+  (->> store/cohorts
+       vals
+       (map :cohort/slug)
+       (into #{})))
+
+(clerk/table
+ (->> (pg/query conn "select method, id, uri, timestamp from access_logs")
+      (filter #(contains? cohort-slugs (uri->cohort-slug (:uri %))))))
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/html [:div {:style {:height "50vh"}}])
