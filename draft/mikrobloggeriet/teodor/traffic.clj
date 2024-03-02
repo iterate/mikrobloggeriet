9dfaca2941c85a8ab834ab1f66a5216ab26903c8
Teodor Heggelund <teodorlu@users.noreply.github.com>
2024-03-02 19:59:13 +0100

    DB sync step 1: server shares access logs table size (#98)
    * create .local for local things
    
    * Ignore .local
    
    * Explore data dumping
    
    * access logs size

--- draft/mikrobloggeriet/teodor/traffic.clj
+++ draft/mikrobloggeriet/teodor/traffic.clj
@@ -13,3 +13,38 @@
 
 (clerk/table
  (pg/query conn "select * from access_logs"))
+
+;; # Sync for access logs
+;;
+;; Motivation.
+;; We want to explore the traffic to Mikrobloggeriet.
+;; To do that, we need to:
+;;
+;; - be able to extract a batch from the traffic logs
+;; - insert that batch of traffic into local traffic logs
+
+(defn dump-traffic-logs [conn]
+  (pg/query conn "select * from access_logs"))
+
+(comment
+  ;; Backup the whole db
+  (doseq [log-line (dump-traffic-logs conn)]
+    (when-let [{:keys [id]} log-line]
+      (spit (str ".local/access-logs-" id ".edn")
+            log-line)))
+  )
+
+;; what is a good batch size?
+
+(count (dump-traffic-logs conn))
+
+;; 29 lines -> 116K storage.
+
+;; mikrobloggeriet: 353 630 lines
+
+(double
+ (* 116 (/ 353630
+           29)))
+
+^{:nextjournal.clerk/visibility {:code :hide}}
+(clerk/html [:div {:style {:height "50vh"}}])
