f81e73629d48b1347ee415396ae351d6dac764e4
Teodor Heggelund <git@teod.eu>
2024-12-10 17:47:51 +0100

    Read cohorts, documents and authors into Datomic

---
+++ src/mblog2/load.clj
@@ -0,0 +1,103 @@
+(ns mblog2.load
+  "Load what we know about cohorts and docs."
+  (:require
+   [babashka.fs :as fs]
+   [clojure.edn :as edn]
+   [datomic.api :as d]
+   [mblog2.schema]))
+
+;; Cohorts and authors are added by hand!
+
+(def cohorts
+  {:cohort/iterate
+   {:cohort/root "text/iterate",
+    :cohort/slug "iterate",
+    :cohort/type :cohort.type/markdown},
+   :cohort/jals
+   {:cohort/root "j", :cohort/slug "jals", :cohort/type :cohort.type/markdown},
+   :cohort/kiel
+   {:cohort/root "text/kiel",
+    :cohort/slug "kiel",
+    :cohort/type :cohort.type/markdown},
+   :cohort/luke
+   {:cohort/root "text/luke",
+    :cohort/slug "luke",
+    :cohort/type :cohort.type/markdown},
+   :cohort/oj
+   {:cohort/root "text/oj",
+    :cohort/slug "oj",
+    :cohort/type :cohort.type/markdown},
+   :cohort/olorm
+   {:cohort/root "o", :cohort/slug "olorm", :cohort/type :cohort.type/markdown},
+   :cohort/urlog
+   {:cohort/root "text/urlog",
+    :cohort/slug "urlog",
+    :cohort/type :cohort.type/urlog},
+   :cohort/vakt
+   {:cohort/root "text/vakt",
+    :cohort/slug "vakt",
+    :cohort/type :cohort.type/markdown}})
+
+(def authors
+  [{:author/email "aaberg89@gmail.com", :author/first-name "Jørgen"}
+   {:author/email "adrian.tofting@iterate.no", :author/first-name "Adrian"}
+   {:author/email "brunstad@iterate.no", :author/first-name "Ole Jacob"}
+   {:author/email "camilla@iterate.no", :author/first-name "Camilla"}
+   {:author/email "ella.swan@iterate.no", :author/first-name "Ella"}
+   {:author/email "finn@iterate.no", :author/first-name "Finn"}
+   {:author/email "git@teod.eu", :author/first-name "Teodor"}
+   {:author/email "haavard@vaage.com", :author/first-name "Håvard"}
+   {:author/email "haugeto@iterate.no", :author/first-name "Anders"}
+   {:author/email "jomarn@me.com", :author/first-name "Johan"}
+   {:author/email "julian.hallen.eriksen@iterate.no", :author/first-name "Julian"}
+   {:author/email "kjersti@iterate.no", :author/first-name "Kjersti"}
+   {:author/email "lars.barlindhaug@iterate.no", :author/first-name "Lars"}
+   {:author/email "larshbj@gmail.com", :author/first-name "Lars"}
+   {:author/email "neno.mindjek@iterate.no", :author/first-name "Neno"}
+   {:author/email "oddmunds@iterate.no", :author/first-name "Oddmund"}
+   {:author/email "olav.moseng@iterate.no", :author/first-name "Olav"}
+   {:author/email "pernille@iterate.no", :author/first-name "Pernille"}
+   {:author/email "rasmus.stride@iterate.no", :author/first-name "Rasmus"}
+   {:author/email "richard.tingstad@iterate.no", :author/first-name "Richard"}
+   {:author/email "rune@iterate.no", :author/first-name "Rune"}
+   {:author/email "sindre@iterate.no", :author/first-name "Sindre"}
+   {:author/email "thusan@iterate.no", :author/first-name "Thusan"}])
+
+(defn find-cohort-docs [cohort]
+  (->> (fs/list-dir (:cohort/root cohort))
+       (filter (fn [dir]
+                 (and (fs/exists? (fs/file dir "index.md"))
+                      (fs/exists? (fs/file dir "meta.edn")))))
+       (map (fn [dir]
+              {:cohort/id (:cohort/id cohort)
+               :cohort/docs
+               (assoc (select-keys (edn/read-string (slurp (fs/file dir "meta.edn")))
+                                   [:doc/created :doc/uuid :git.user/email])
+                      :doc/markdown (slurp (fs/file dir "index.md")))}))))
+
+(defn loaddb [cohorts authors]
+  (let [uri (str "datomic:mem://" (random-uuid))
+        _ (d/create-database uri)
+        conn (d/connect uri)]
+    @(d/transact conn mblog2.schema/schema)
+    @(d/transact conn
+                 (for [[cohort-id cohort] cohorts]
+                   (assoc cohort :cohort/id cohort-id)))
+    @(d/transact conn authors)
+    (doseq [cohort-id
+            (d/q '[:find [?e ...]
+                   :where [?e :cohort/id]]
+                 (d/db conn))]
+      (d/transact conn
+                  (find-cohort-docs
+                   (d/entity (d/db conn) cohort-id))))
+    (d/db conn)))
+
+(comment
+
+  (def db (loaddb cohorts authors))
+  (def olorm (d/entity db [:cohort/id :cohort/olorm]))
+
+  (:cohort/docs olorm)
+  
+  )
---
+++ src/mblog2/schema.clj
@@ -0,0 +1,69 @@
+(ns mblog2.schema)
+
+(def schema
+  [;; Cohorts
+   {:db/ident :cohort/id
+    :db/valueType :db.type/keyword
+    :db/cardinality :db.cardinality/one
+    :db/unique :db.unique/identity}
+
+   {:db/ident :cohort/root
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/unique :db.unique/identity}
+
+   {:db/ident :cohort/slug
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/unique :db.unique/identity}
+
+   {:db/ident :cohort/type
+    :db/valueType :db.type/keyword
+    :db/cardinality :db.cardinality/one}
+
+   ;; Authors
+   {:db/ident :author/email
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/unique :db.unique/identity}
+
+   {:db/ident :author/first-name
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+
+   ;; Docs
+   {:db/ident :doc/slug
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :doc/created
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :doc/uuid
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one
+    :db/unique :db.unique/identity}
+
+   {:db/ident :git.user/email
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :doc/markdown
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+
+   {:db/ident :doc/html
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+
+   ;; Doc author
+   {:db/ident :doc/primary-author
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/one}
+
+   ;; Cohort docs
+   {:db/ident :cohort/docs
+    :db/valueType :db.type/ref
+    :db/cardinality :db.cardinality/many}
+   ])
