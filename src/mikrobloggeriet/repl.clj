7c0eba48761b2ab77b3e9f3e877d01c9c205e5fa
Olav Haaland Moseng <42978548+olavm@users.noreply.github.com>
2024-12-16 16:44:49 +0100

    Add example usage of datomic

--- src/mikrobloggeriet/repl.clj
+++ src/mikrobloggeriet/repl.clj
@@ -1,3 +1,14 @@
 (ns mikrobloggeriet.repl)
 
 (defonce state (atom nil))
+
+(comment
+  (def db (:mikrobloggeriet.system/datomic @state))
+  (require '[datomic.api :as d])
+
+  (d/q '[:find ?slug
+         :where [_ :doc/slug ?slug]]
+       db)
+  :rcf)
+
+
