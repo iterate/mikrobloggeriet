2605c7818d8dd1a7861aded57757338215562c13
Teodor Heggelund <git@teod.eu>
2024-12-21 23:50:05 +0100

    Fjern ubrukt kode i mikrobloggeriet.cohort

--- test/mikrobloggeriet/cohort_test.clj
+++ test/mikrobloggeriet/cohort_test.clj
@@ -1,17 +1,7 @@
 (ns mikrobloggeriet.cohort-test
   (:require
    [clojure.test :refer [deftest is]]
-   [mikrobloggeriet.cohort :as cohort]
-   [mikrobloggeriet.cohort.markdown :as cohort.markdown]
-   [mikrobloggeriet.store :as store]))
-
-(deftest slug-test
-  (is (= "oj" (cohort.markdown/slug store/oj)))
-  (is (nil? (cohort.markdown/slug {}))))
-
-(deftest name-test
-  (is (= "OLORM"
-         (cohort.markdown/name store/olorm))))
+   [mikrobloggeriet.cohort :as cohort]))
 
 (deftest href-test
   (is (= "/urlog/"
