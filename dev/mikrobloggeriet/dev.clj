e63faaeea35bad864be031ba3771828d6eaff609
Teodor Heggelund <git@teod.eu>
2024-12-21 20:50:17 +0100

    Lag utkast til deploy direkte fra REPL

--- dev/mikrobloggeriet/dev.clj
+++ dev/mikrobloggeriet/dev.clj
@@ -1,6 +1,14 @@
-(ns mikrobloggeriet.dev)
+(ns mikrobloggeriet.dev
+  (:require
+   [babashka.process :as p]))
 
-(defn garden-deploy! [])
+(defn garden-deploy!
+  "Idea: why don't we just deploy from a connected REPL?"
+  []
+  ;; TODO run the tests
+  ;; TDOO check that we're on master
+  ;; IN THAT CASE,
+  (comment (p/shell "garden deploy")))
 
 (comment ;; s-:
   (garden-deploy!)
