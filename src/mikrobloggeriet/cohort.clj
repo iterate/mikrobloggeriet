56ebdf706da23464159b0539423584c6f418543e
Teodor Heggelund <git@teod.eu>
2024-12-22 17:25:29 +0100

    Add function to get all cohorts

--- src/mikrobloggeriet/cohort.clj
+++ src/mikrobloggeriet/cohort.clj
@@ -1,5 +1,14 @@
-(ns mikrobloggeriet.cohort)
+(ns mikrobloggeriet.cohort
+  (:require
+   [datomic.api :as d]))
 
 (defn href [cohort]
   (when-let [slug (:cohort/slug cohort)]
     (str "/" slug "/")))
+
+(defn all-cohorts [db]
+  (for [cohort-id
+        (d/q '[:find [?id ...]
+               :where [_ :cohort/id ?id]]
+             db)]
+    (d/entity db [:cohort/id cohort-id])))
--- test/mikrobloggeriet/cohort_test.clj
+++ test/mikrobloggeriet/cohort_test.clj
@@ -1,10 +1,20 @@
 (ns mikrobloggeriet.cohort-test
   (:require
    [clojure.test :refer [deftest is testing]]
-   [mikrobloggeriet.cohort :as cohort]))
+   [mikrobloggeriet.cohort :as cohort]
+   [mikrobloggeriet.db :as db]))
 
 (deftest href-test
   (is (= "/urlog/"
          (cohort/href {:cohort/slug "urlog"})))
   (testing "Returnerer nil hvis vi ikke kjenner slug"
     (is (nil? (cohort/href {})))))
+
+(def db (db/loaddb {:cohorts db/cohorts :authors db/authors}))
+
+(deftest all-cohorts-test
+  (let [cohorts (cohort/all-cohorts db)]
+    (is (contains? (->> cohorts
+                        (map :cohort/id)
+                        (into #{}))
+                   :cohort/olorm))))
