2b280346b530d2aa1a1d558f075c84f6fcbb628a
Teodor Heggelund <git@teod.eu>
2025-03-19 18:46:58 +0100

    Lanser LEIK - aka finn created date på fil

--- src/mblog/leik.clj
+++ src/mblog/leik.clj
@@ -1,15 +1,19 @@
 (ns mblog.leik
   "en ekstra leken kohort"
   (:require
-   [babashka.fs :as fs]))
+   [babashka.fs :as fs]
+   [clojure.string :as str]))
+
+(defn created-date [f]
+  (some-> (fs/creation-time f)
+          str (str/split #"T") first))
 
 (defn load-doc [f]
-  (when-let [number (first (re-matches #"(\d+)\.md" (fs/file-name f)))]
-    {:doc/slug (str "leik-" number)
-     :doc/markdown (slurp (fs/file f))
-     ;;     :doc/created "2099-12-12"
-     }
-    ))
+  (when (fs/exists? f)
+    (when-let [number (first (re-matches #"(\d+)\.md" (fs/file-name f)))]
+      {:doc/slug (str "leik-" number)
+       :doc/markdown (slurp (fs/file f))
+       :doc/created (created-date f)})))
 
 (defn find-docs []
   (->> (fs/list-dir "text/leik")
@@ -20,4 +24,8 @@
   (def f "text/leik/1.md")
   (fs/file-name f)
   (load-doc f)
+  (some-> (fs/creation-time "text/leik/1.md")
+          str
+          (str/split #"T")
+          first)
   )
