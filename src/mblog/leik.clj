eef3be53f3781fe08439b7a928089bd4695e7cf9
Teodor Heggelund <git@teod.eu>
2025-03-19 18:34:23 +0100

    Husk å legge til koden 🙈

---
+++ src/mblog/leik.clj
@@ -0,0 +1,23 @@
+(ns mblog.leik
+  "en mer leken kohort enn de andre"
+  (:require
+   [babashka.fs :as fs]))
+
+(defn load-doc [f]
+  (when-let [number (first (re-matches #"(\d+)\.md" (fs/file-name f)))]
+    {:doc/slug (str "leik-" number)
+     :doc/markdown (slurp (fs/file f))
+     ;;     :doc/created "2099-12-12"
+     }
+    ))
+
+(defn find-docs []
+  (->> (fs/list-dir "text/leik")
+      (keep load-doc)))
+
+(comment
+  (find-docs)
+  (def f "text/leik/1.md")
+  (fs/file-name f)
+  (load-doc f)
+  )
