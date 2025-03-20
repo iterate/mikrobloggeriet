820649445fe9ea9fbc2ee02c8b9afb1d4be17ffa
Olav Haaland Moseng <42978548+olavm@users.noreply.github.com>
2025-03-20 19:01:00 +0100

    Make test more readable

--- test/mblog/indigo_test.clj
+++ test/mblog/indigo_test.clj
@@ -18,32 +18,24 @@
     --background01: #080D92;
     --white100: #fff;
     --white80: rgba(255, 255, 255, 0.8);
-    --white50: rgba(255, 255, 255, 0.5);
-    --white20: rgba(255, 255, 255, 0.2);
-    --white10: rgba(255, 255, 255, 0.1);
     --black100: #000;
-    --black80: rgba(0, 0, 0, 0.8);
-    --black50: rgba(0, 0, 0, 0.5);
-    --black20: rgba(0, 0, 0, 0.2);
-    --black10: rgba(0, 0, 0, 0.1);
   }
-
-     h1, h2, h3, h4, h5, h6, p, li, figcaption, aside {
+     h1, h2 {
      color: var(--white100);
-     text-decoration: none;
      font-style: normal;
      font-family: 'IBM Plex Mono', monospace;
-     font-weight: 300;
-     font-style: normal;
      font-size: clamp(1rem, 1vw, 1.5rem);
      line-height: 150%;}"]
 
     (is (= "IBM Plex Mono, monospace"
            (indigo/css->font css-str)))
+
     (is (= "#080D92"
            (indigo/css->background-color css-str)))
+
     (is (= "#fff"
            (indigo/css->text-color css-str)))
+
     (is (nil? (indigo/css->text-color "ugyldig css")))))
 
 (deftest left-bar
