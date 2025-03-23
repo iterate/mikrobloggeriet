eba765476e7eb3af1d4945a9285474efb5e06868
Olav Haaland Moseng <42978548+olavm@users.noreply.github.com>
2025-03-23 22:29:13 +0100

    More optimalisation on the recur func: remove unneccessary steps

--- src/mikrobloggeriet/contrast.clj
+++ src/mikrobloggeriet/contrast.clj
@@ -22,24 +22,53 @@
 (defn adjust-gamma [_]
   (math/pow (/ (+ _ 0.055) 1.055) 2.4))
 
-(defn relative-luminance [rgb]
-  (let [rsrgb (/ (rgb 0) 255)
-        gsrgb (/ (rgb 1) 255)
-        bsrgb (/ (rgb 2) 255)
+(defn relative-luminance
+  "The relative brightness of any point in a colorspace, normalized to 0 for darkest black and 1 for lightest white"
+  [[r g b]]
+  (let [rsrgb (/ r 255)
+        gsrgb (/ g 255)
+        bsrgb (/ b 255)
         r (if (<= rsrgb 0.03923) (* rsrgb lowc) (adjust-gamma rsrgb))
         g (if (<= gsrgb 0.03923) (* gsrgb lowc) (adjust-gamma gsrgb))
         b (if (<= bsrgb 0.03923) (* bsrgb lowc) (adjust-gamma bsrgb))]
     (+ (* r rc) (* g gc) (* b bc))))
 
-(defn luminance [a b]
+(defn contrast-ratio [a b]
   (let [l1 (max a b)
         l2 (min a b)]
     (/ (+ l1 0.05) (+ l2 0.05))))
 
-(defn rbg->score [a b]
-  (luminance (relative-luminance a) (relative-luminance b)))
+(defn rgb->score
+  "Calculate the contrast ratio between two relative luminance values"
+  [c1 c2]
+  (contrast-ratio (relative-luminance c1) (relative-luminance c2)))
 
-;; Get the contrast ratio between two relative luminance values
+(defn rgb->hex [[r g b]]
+  (format "#%02X%02X%02X" r g b))
+
+(defn rand-rgb []
+  [(rand-int 256) (rand-int 256) (rand-int 256)])
+
+(defn gen-colors
+  "Generate two random colors with a contrast radio over a given threshold.
+   Contrast ratios can range from 1 to 21.
+   To ensure performance, the the threshold is capped at 18."
+  [threshold]
+  (if (> threshold 18) nil
+      (loop [c1 (rand-rgb)
+             c2 (rand-rgb)
+             iter 0]
+        (let [score (rgb->score c1 c2)]
+          (if (> score threshold)
+            {:c1 (rgb->hex c1)
+             :c2 (rgb->hex c2)
+             :score score
+             :iter iter}
+            (recur c2 (rand-rgb) (inc iter)))))))
+
+(defn rand-hex-color []
+  (str "#" (format "%06x" (rand-int 16777215))))
+
 (defn hex->rgb [hex]
   (let [hex (str/replace hex #"^#" "")
         n (Integer/parseInt hex 16)
@@ -47,19 +76,3 @@
         green (bit-and (bit-shift-right n 8) 255)
         blue (bit-and n 255)]
     [red green blue]))
-
-;; Get contrast score from hex-values
-(defn hex->score [a b]
-  (rbg->score (hex->rgb a) (hex->rgb b)))
-
-(defn rand-hex-color []
-  (str "#" (format "%06x" (rand-int 16777215))))
-
-(defn gen-colors [threshold]
-  (loop [c1 (rand-hex-color)
-         c2 (rand-hex-color)
-         iter 0]
-    (let [score (hex->score c1 c2)]
-      (if (> score threshold)
-        {:c1 c1 :c2 c2 :score score :iter iter}
-        (recur c2 (rand-hex-color) (inc iter))))))
--- test/mikrobloggeriet/contrast_test.clj
+++ test/mikrobloggeriet/contrast_test.clj
@@ -9,20 +9,25 @@
 
 (deftest luminance
   (is (= 21.0
-         (contrast/luminance (contrast/relative-luminance [0 0 0])
+         (contrast/contrast-ratio (contrast/relative-luminance [0 0 0])
                              (contrast/relative-luminance [255 255 255]))))
 
-  (is (= 21.0 (contrast/rbg->score [0 0 0] [255 255 255])))
-
-  (is (= 1.0 (contrast/luminance 1 1))))
+  (is (= 21.0 (contrast/rgb->score [0 0 0] [255 255 255])))
+  (is (= 1.0 (contrast/contrast-ratio 1 1))))
 
 (deftest hex->rgb
   (is (= [0 0 0] (contrast/hex->rgb "#00000")))
   (is (= [255 255 255] (contrast/hex->rgb "#ffffff"))))
 
+(deftest rgb->hex
+  (is (= "#00BFFF" (contrast/rgb->hex [0 191 255])))
+  (is (= "#FFCCFF" (contrast/rgb->hex [255 204 255])))
+  (let [rand-color (contrast/rand-rgb)]
+    (is (= rand-color
+           (contrast/hex->rgb (contrast/rgb->hex rand-color))))))
+
 (deftest scoring
-  (is (= 21.0 (contrast/rbg->score [0 0 0] [255 255 255])))
-  (is (= 21.0 (contrast/hex->score "#00000" "#ffffff"))))
+  (is (= 21.0 (contrast/rgb->score [0 0 0] [255 255 255]))))
 
 (deftest random-hex-color
   (let [rand-hex (contrast/rand-hex-color)]
@@ -32,4 +37,6 @@
 (deftest generate-contrasting-colors
   (let [threshold 7
         colors (contrast/gen-colors threshold)]
-    (is (< 7 (contrast/hex->score (:c1 colors) (:c2 colors))))))
+    (is (< threshold
+           (contrast/rgb->score (contrast/hex->rgb (:c1 colors))
+                                (contrast/hex->rgb (:c2 colors)))))))
