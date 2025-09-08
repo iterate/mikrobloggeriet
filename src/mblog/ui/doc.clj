35b9639a79b5c18452b70a3751f0e074fcf6aedf
Olav Haaland Moseng <42978548+olavm@users.noreply.github.com>
2025-09-08 20:51:34 +0200

    Begrens avsnittsbredde i enkeltdokumenter

--- src/mblog/ui/doc.clj
+++ src/mblog/ui/doc.clj
@@ -3,17 +3,26 @@
    [datomic.api :as d]
    [mikrobloggeriet.doc :as doc]))
 
+(def samvirk
+  {:bg-color "#91c1e9"
+   :text-color "#1a2c5b"
+   :font "font16.css"
+   :root ":root {\n   --first100: rgb(145,193,233);\n   --first80: rgba(145,193,233, 0.8);\n   --first50: rgba(145,193,233, 0.5);\n   --first20: rgba(145,193,233, 0.2);\n   --first10: rgba(145,193,233, 0.1);\n   --second100: rgb(26,44,91);\n   --second80: rgba(26,44,91, 0.8);\n   --second50: rgba(26,44,91, 0.5);\n   --second20: rgba(26,44,91, 0.2);\n   --second10: rgba(26,44,91, 0.1);\n}"})
+
 (defn doc->hiccup [doc]
   [:html {:lang "en"}
    [:head
     [:meta {:charset "utf-8"}]
-    [:link {:rel "stylesheet" :href "/css/styles/indigo.css"}]]
+    [:link {:rel "stylesheet" :href "/css/styles/indigo.css"}]
+    [:style (:root samvirk)]
+    [:style ""]]
    [:body
-    [:header
-     [:a {:href "/"}
-      [:h1 "Mikrobloggeriet"]]]
-    [:div
-     (-> doc doc/hiccup)]]])
+    [:container {:style "p {max-width: 60rem};"}
+     [:header
+      [:a {:href "/"}
+       [:p "Mikrobloggeriet"]]]
+     [:div
+      (-> doc doc/hiccup)]]]])
 
 (defn req->doc [req]
   (d/entity (:mikrobloggeriet.system/datomic req)
