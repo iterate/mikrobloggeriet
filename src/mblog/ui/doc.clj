ea63d6ca4aa81a795673a88d9734d1f20d1af65e
Teodor Heggelund <git@teod.eu>
2025-09-18 20:46:49 +0200

    Hei alle som jobber med mikrobloggeriet!
    Nå har vi fikset fargene for dere, værsågod!
    
     Xoxo, Ingvild og Teodor

--- src/mblog/ui/doc.clj
+++ src/mblog/ui/doc.clj
@@ -3,7 +3,33 @@
    [datomic.api :as d]
    [mikrobloggeriet.doc :as doc]))
 
-(def css-vars ":root {\n   --first100: rgb(145,193,233);\n   --first80: rgba(145,193,233, 0.8);\n   --first50: rgba(145,193,233, 0.5);\n   --first20: rgba(145,193,233, 0.2);\n   --first10: rgba(145,193,233, 0.1);\n   --second100: rgb(26,44,91);\n   --second80: rgba(26,44,91, 0.8);\n   --second50: rgba(26,44,91, 0.5);\n   --second20: rgba(26,44,91, 0.2);\n   --second10: rgba(26,44,91, 0.1);\n}")
+;; (def css-vars ":root {\n   --first100: rgb(145,193,233);\n   --first80: rgba(145,193,233, 0.8);\n   --first50: rgba(145,193,233, 0.5);\n   --first20: rgba(145,193,233, 0.2);\n   --first10: rgba(145,193,233, 0.1);\n   --second100: rgb(26,44,91);\n   --second80: rgba(26,44,91, 0.8);\n   --second50: rgba(26,44,91, 0.5);\n   --second20: rgba(26,44,91, 0.2);\n   --second10: rgba(26,44,91, 0.1);\n}")
+
+
+(def css-vars ":root {
+
+   --first80: rgba(145,193,233, 0.8);
+   --first50: rgba(145,193,233, 0.5);
+   --first20: rgba(145,193,233, 0.2);
+   --first10: rgba(145,193,233, 0.1);
+   --second100: rgb(26,44,91);
+   --second80: rgba(26,44,91, 0.8);
+   --second50: rgba(26,44,91, 0.5);
+   --second20: rgba(26,44,91, 0.2);
+   --second10: rgba(26,44,91, 0.1);
+}")
+
+
+(def bringebær "rgb(198, 12, 90)")
+
+(def grønn "#a0fb6c")
+
+(def more-css-vars
+  (str ":root {"
+       "--first100: " bringebær ";"
+       "--second100: " grønn ";"
+       "}"))
+
 
 (defn doc->hiccup [doc]
   [:html {:lang "en"}
@@ -11,6 +37,7 @@
     [:meta {:charset "utf-8"}]
     [:link {:rel "stylesheet" :href "/css/styles/doc.css"}]
     [:style css-vars]
+    [:style more-css-vars]
     [:style "p {max-width: 60rem};"]]
    [:body
     [:div
